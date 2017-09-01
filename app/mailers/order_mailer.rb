f97a704e2c901413981cddc7004e5eb091275334
Hannah Kim <h.heeeun.kim@gmail.com>
2017-09-01 19:32:26 +0000

    added order-receipt feature and able to preview it

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,3 +1,9 @@
 class OrderMailer < ApplicationMailer
   default from: 'no-reply@jungle.com'
+
+  def order_receipt(order)
+    @order = order
+    @url = '/orders'
+    mail(to: @order.email, subject: "Your order #{@order.id} has been received.")
+  end
 end
---
+++ test/mailers/previews/order_mailer_preview.rb
@@ -0,0 +1,5 @@
+class OrderMailerPreview < ActionMailer::Preview
+  def order_receipt
+    OrderMailer.order_receipt(Order.first)
+  end
+end
